ee3535b7bec7af80063a9c6979230a2bf39eae37
NeckitWin <neckit.v@gmail.com>
2025-03-06 22:00:09 +0100

    feat: visual drag and move taks item

--- src/components/tasks/TaskItem.tsx
+++ src/components/tasks/TaskItem.tsx
@@ -3,6 +3,8 @@ import {useAppDispatch} from "../../app/hooks.ts";
 import {removeTaskFromTable} from "../../features/tableSlice.ts";
 import {removeTask, setCurrentTask} from "../../features/taskSlice.ts";
 import {openModal} from "../../features/modalSlice.ts";
+import {useDraggable} from "@dnd-kit/core";
+import {CSS} from '@dnd-kit/utilities';
 
 interface TaskItemProps {
     tableId: number;
@@ -11,6 +13,14 @@ interface TaskItemProps {
 
 const TaskItem = ({task, tableId}: TaskItemProps) => {
     const dispatch = useAppDispatch();
+
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: task.id
+    })
+
+    const style = {
+        transform: CSS.Translate.toString(transform)
+    }
 
     const openTaskInfo = () => {
         dispatch(setCurrentTask(task));
@@ -23,25 +33,24 @@ const TaskItem = ({task, tableId}: TaskItemProps) => {
     };
 
     return (
-        <>
-            <div className="p-4 border border-white/10 rounded-lg shadow-sm flex justify-between">
-                <div>
-                    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-white">{task.title}</h5>
-                    <p className="mb-3 font-normal text-gray-400">{task.description}</p>
-                </div>
-                <div className='flex flex-col justify-around'>
-                    <button>
-                        <img src="svg/move.svg" alt="move" className='w-4 h-4 cursor-pointer'/>
-                    </button>
-                    <button onClick={openTaskInfo}>
-                        <img src="svg/info.svg" alt="info" className='w-4 h-4 cursor-pointer'/>
-                    </button>
-                    <button onClick={deleteTask}>
-                        <img src="svg/bin.svg" alt="bin" className='w-4 h-4 cursor-pointer'/>
-                    </button>
-                </div>
+        <div className="p-4 border border-white/10 rounded-lg shadow-sm flex justify-between"
+             style={style} ref={setNodeRef} >
+            <div>
+                <h5 className="mb-2 text-2xl font-semibold tracking-tight text-white">{task.title}</h5>
+                <p className="mb-3 font-normal text-gray-400">{task.description}</p>
             </div>
-        </>
+            <div className='flex flex-col justify-around'>
+                <button {...listeners} {...attributes}>
+                    <img src="svg/move.svg" alt="move" className='w-4 h-4 cursor-pointer'/>
+                </button>
+                <button onClick={openTaskInfo}>
+                    <img src="svg/info.svg" alt="info" className='w-4 h-4 cursor-pointer'/>
+                </button>
+                <button onClick={deleteTask}>
+                    <img src="svg/bin.svg" alt="bin" className='w-4 h-4 cursor-pointer'/>
+                </button>
+            </div>
+        </div>
     );
 };
 
